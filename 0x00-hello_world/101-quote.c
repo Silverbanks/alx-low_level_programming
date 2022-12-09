549fd7df76d297d7ffd62f62897245ca10cc2be4
Silverbanks <silver127@gmx.us>
2022-12-08 19:12:04 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 
-#include <string.h>
 
 #include <unistd.h>
 
 /**
  * main - Main program
  *
- * Return: 1
+ * Return: Always 0 (success)
  */
 
 int main(void)
 
 {
 
-       write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+       write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
        return (1);	
 
 }
