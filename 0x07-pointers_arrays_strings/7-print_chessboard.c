9edc9555f851154f287de4a3baabfef5951eb1e2
Silverbanks <silver127@gmx.us>
2023-01-02 15:34:24 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,14 +3,14 @@
 /**
  * print_chessboard - prints 2D array of chessboard
  * @a: chessboard array
- * Retrun: void
+ * Return: void
  */
 
 void print_chessboard(char (*a)[8])
 {
 	int row, col;
 
-	for (row = 0; < 8; col++)
+	for (row = 0; row < 8; row++)
 	{
 		for (col = 0; col < 8; col++)
 		{
