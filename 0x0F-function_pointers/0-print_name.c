a94486388440567496f2ab37c83abd3b743f4743
Silverbanks <silver127@gmx.us>
2023-01-22 23:49:48 -0800

    3-main.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,15 @@
-#include <stdio.h>
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: pointer to the printing function
- * Return: void
- */
-
+ * print_name - function that prints name
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
-
 {
 	if (name == NULL || f == NULL)
-
-	{
 		return;
-	}
+
 	f(name);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,40 +2,42 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
-* main - Prints calculator result
-*
-* @argc: int of commandline args
-*
-* @argv: 2-d array of commandline args
-*
-* Return: 0 success, 98 for argc error, 100 if div 0
-*/
-
-int main(int argc, char **argv)
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int (*operator_function)(int, int), num1, num2;
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
-		printf("Error\n"), exit(98);
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	num1 = atoi(argv[1]);
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	operator_function = get_op_func(argv[2]);
-	if (!operator_function)
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if (!num2 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", operator_function(num1, num2));
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
