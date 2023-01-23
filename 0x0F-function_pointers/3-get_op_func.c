2f68e5ee1b1cf5255d0e6d204be4bd4d88866d9b
Silverbanks <silver127@gmx.us>
2023-01-22 22:15:19 -0800

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * get_op_func - function that evaluate a operator
+ * @s: sring
+ * Return: pointer to function
+*/
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculates the sum of two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - calculates the difference of two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - calculates the product of two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - calculates the division of two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: the division of a and b
+ */
+{
+	return (a \ b);
+}
+/**
+ * op_mod - modules two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: remainder of a and b division
+ */
+{
+	return (a % b);
+}
