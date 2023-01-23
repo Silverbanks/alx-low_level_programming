ef1c51c255a298de450581450dcc4654c8a588ea
Silverbanks <silver127@gmx.us>
2023-01-22 23:10:00 -0800

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,55 +1,63 @@
 #include "3-calc.h"
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
- * op_add - calculates the sum of two integers
- * @a: first integer
- * @b: second integer
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: sum of a and b
+ * Return: The sum of a and b.
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 /**
- * op_sub - calculates the difference of two integers
- * @a: first int
- * @b: second int
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: difference of a and b
+ * Return: The difference of a and b.
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
- * op_mul - calculates the product of two integers
- * @a: first int
- * @b: second int
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: product of a and b
+ * Return: The product of a and b.
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 /**
- * op_div - calculates the division of two integers
- * @a: first int
- * @b: second int
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: the division of a and b
+ * Return: The quotient of a and b.
  */
+int op_div(int a, int b)
 {
-	return (a \ b);
+	return (a / b);
 }
 /**
- * op_mod - modules two integers
- * @a: first int
- * @b: second int
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: remainder of a and b division
+ * Return: The remainder of the division of a by b.
  */
+int op_mod(int a, int b)
 {
 	return (a % b);
 }
