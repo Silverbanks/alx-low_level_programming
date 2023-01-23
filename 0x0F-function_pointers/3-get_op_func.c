f577b0822f77dd4d6b8ece4f60c4e2a1d4aac525
Silverbanks <silver127@gmx.us>
2023-01-23 00:14:21 -0800

    3-get_op_func.c, 3-op_functions.c, 3-calc.h

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,7 +3,7 @@
 #include <string.h>
 /**
  * get_op_func - function that evaluate a operator
- * @s: sring
+ * @s: string
  * Return: pointer to function
 */
 int (*get_op_func(char *s))(int, int)
@@ -14,15 +14,13 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}};
+		{NULL, NULL},
+	};
+
 	int i = 0;
 
-	while (i < 5)
-	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	printf("Error\n");
-	exit(99);
+
+	return (ops[i].f);
 }
